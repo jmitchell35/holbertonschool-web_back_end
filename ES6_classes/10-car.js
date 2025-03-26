8a579717a348e1b30dbf29060f3ee9afb77d981f
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-26 11:55:48 +0100

    Simpler version skipping input validation

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,78 +1,42 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    }
-
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
-    }
-
-    if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-
-    Object.defineProperty(this, 'id', {
-      value: Symbol('id'),
-      enumerable: false
-    });
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(newMotor) {
-    if (typeof newMotor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    }
-
-    this._motor = newMotor;
   }
 
   get brand() {
     return this._brand;
   }
 
-  set brand(newBrand) {
-    if (typeof newBrand !== 'string') {
-      throw new TypeError('Brand must be a string');
-    }
+  set brand(value) {
+    this._brand = value;
+  }
 
-    this._brand = newBrand;
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
   }
 
   get color() {
     return this._color;
   }
 
-  set color(newColor) {
-    if (typeof newColor !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
-
-    this._color = newColor;
+  set color(value) {
+    this._color = value;
   }
 
   cloneCar() {
     const Constructor = this.constructor;
     const newCar = Object.create(Constructor.prototype);
-    // eslint-disable-next-line no-underscore-dangle
+    
     newCar._brand = undefined;
-    // eslint-disable-next-line no-underscore-dangle
     newCar._motor = undefined;
-    // eslint-disable-next-line no-underscore-dangle
     newCar._color = undefined;
-
-    Object.defineProperty(newCar, 'id', {
-      value: Symbol('id'),
-      enumerable: false  // This makes it invisible in console.log
-    });
-
+    
     return newCar;
   }
 }
