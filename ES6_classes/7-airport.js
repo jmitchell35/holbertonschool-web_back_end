7a9aef0ae2233d8d97b6c995f522abe3ba06df0f
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-26 08:43:31 +0100

    Task 7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,42 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+
+    this._name = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = newName;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
