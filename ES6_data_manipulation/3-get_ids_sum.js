b750ccb1cf04434a4073fc8fead44c06306e8461
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-07 16:59:18 +0200

    Adding missing semi-colon

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(stdList) {
-  return stdList.reduce((sum, student) => sum + student.id, 0)
+  return stdList.reduce((sum, student) => sum + student.id, 0);
 }
