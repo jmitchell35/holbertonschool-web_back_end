240390bba647683181c6ea7b1e6a07a9e7110690
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-07 21:42:29 +0200

    eslint-compliant

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -4,12 +4,9 @@ export default function cleanSet(set, startString) {
     return '';
   }
 
-  return [...set]  // Convert Set to array with constructor, method chaining structure
-    .filter(value => 
-      typeof value === 'string' && value.startsWith(startString)
-    )  // Keep only strings that start with startString
-    .map(value => 
-      value.slice(startString.length)
-    )  // Remove the startString prefix
-    .join('-');  // Join with hyphens
+  return [...set] // Convert Set to array with constructor, method chaining structure
+    // Keep only strings that start with startString
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length)) // Remove the startString prefix
+    .join('-'); // Join with hyphens
 }
