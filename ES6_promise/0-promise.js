3b0fd9ba0e90ffdf223086190e0a9c1830442620
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-08 12:42:52 +0200

    Task 0 - 1st

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolveOuter) => {
+    resolveOuter(
+      new Promise((resolveInner) => {
+        setTimeout(resolveInner, 1000);
+      }),
+    );
+  });
+}
