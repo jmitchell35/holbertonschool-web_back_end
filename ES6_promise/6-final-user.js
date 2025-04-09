4bfef7be5ffad914c0c197d24fcb7400e8b9e748
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-09 11:54:33 +0200

    Switching function call order

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,13 +4,15 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([
-      uploadPhoto(fileName),
       signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
     ])
     .then(
       (results) => results.map((result) => ({ // results is an arr of results we map into iterable
         status: result.status, // Unifies obj structure for fulfilled and rejected promises
-        value: result.status === 'fulfilled' ? result.value : result.reason, // from above
+        value: result.status === 'fulfilled' // hence the status check
+        ? result.value // fulfilled promise output (status, value pairs)
+        : result.reason, // rejected promise output (status, reason pairs)
       })),
     );
 }
