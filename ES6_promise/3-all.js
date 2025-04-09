bdff8b9baa4f809a53e7009183abafbe3b674209
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-09 10:53:29 +0200

    Adding error handler (catch line 11)

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -7,5 +7,10 @@ export default function handleProfileSignup() {
       ([photo, user]) => {
         console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
       },
+    )
+    .catch(
+      () => {
+        console.log('Signup system offline');
+      },
     );
 }
