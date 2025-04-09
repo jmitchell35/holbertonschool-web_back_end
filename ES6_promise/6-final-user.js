effe70d4450a4f729a6e1ebd1970c58f3ce42eb4
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-09 11:57:54 +0200

    Trying to fix error format

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -12,7 +12,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
         status: result.status, // Unifies obj structure for fulfilled and rejected promises
         value: result.status === 'fulfilled' // hence the status check
           ? result.value // fulfilled promise output (status, value pairs)
-          : result.reason, // rejected promise output (status, reason pairs)
+          : `Error: ${result.reason.message}`, // rejected promise output (status, reason pairs)
       })),
     );
 }
