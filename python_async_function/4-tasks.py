58088aa193598c48cf209dfc9619cafddcb2c78b
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-23 11:24:20 +0200

    Using working task 1

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
-"""Module that provides an asynchronous function using Tasks."""
+"""Module that provides an asynchronous function to wait for n random delays.
+"""
 import asyncio
+from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawns task_wait_random n times with the specified max_delay.
-    
+    Spawns wait_random n times with the specified max_delay.
+
     Args:
-        n: Number of times to spawn task_wait_random.
+        n: Number of times to spawn wait_random.
         max_delay: Maximum delay in seconds.
-        
+
     Returns:
         List of all the delays in ascending order.
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return [await coroutine for coroutine in asyncio.as_completed(tasks)]
+    # List of scheduled tasks here, instead of unscheduled coroutines.
+    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+
+    delays = []
+    # Await in comprehension can be problematic => hence below
+    for future in asyncio.as_completed(tasks):
+        result = await future
+        delays.append(result)
+
+    return delays
