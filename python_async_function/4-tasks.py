2049282355cd3f6031fd0b059f9b648cd338b6f6
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-23 11:26:40 +0200

    Fixing function name

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns wait_random n times with the specified max_delay.
 
