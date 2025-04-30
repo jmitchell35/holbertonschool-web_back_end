f81b88df43aadc3d214ac4b194a8ca00bcf73051
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-30 20:25:42 +0200

    pycodestyle-compliant

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,23 +2,26 @@
 """
 A module demonstrating asynchronous generators in Python.
 
-This module provides utilities for generating asynchronous sequences of random values.
+This module provides utilities for generating asynchronous sequences of random
+values.
 """
 
 import asyncio
 import random
 from typing import Generator
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """
     Generate a sequence of random float values asynchronously.
-    
+
     This coroutine function yields 10 random float values between 0 and 10,
     with a 1-second delay between each value.
-    
+
     Returns:
-        Generator[float, None, None]: An asynchronous generator yielding random floats.
-    
+        Generator[float, None, None]: An asynchronous generator yielding random
+        floats.
+
     Example:
         >>> async for value in async_generator():
         ...     print(value)
