f7230f3336ddc2d113092f10a81f58d0391f8493
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-21 12:51:37 +0200

    Removing encoding

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,6 @@ const displayMessage = require('./0-console');
 
 
 displayMessage('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', function() {
   var chunk = process.stdin.read();
