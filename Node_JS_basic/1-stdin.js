a380f116ec0caca47c2b77821ccaaa6ca2c4522c
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-21 12:56:01 +0200

    Trying arrow function syntax instead of function, removed !==

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk.trim()}`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
