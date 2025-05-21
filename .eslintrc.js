03183dfa89553e80ca62e8f5604a238db68e2793
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-21 11:59:21 +0200

    Config, readme, task 0

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
   },
   plugins: ['jest'],
   rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
     'no-console': 'off',
     'no-shadow': 'off',
     'no-restricted-syntax': [
@@ -25,10 +27,6 @@ module.exports = {
       'LabeledStatement',
       'WithStatement',
     ],
-    "no-underscore-dangle": [
-      "error",
-      { "allowAfterThis": true }
-    ]
   },
   overrides:[
     {
---
+++ Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+Le SUPER readme
