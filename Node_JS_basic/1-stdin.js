0ddc87c35d04090b7949fcfafff4b025881ac870
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-21 12:46:57 +0200

    No function built

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,18 @@
 const displayMessage = require('./0-console');
 
-function prompt() {
-  displayMessage('Welcome to Holberton School, what is your name?');
-  process.stdin.setEncoding('utf8');
 
-  process.stdin.on('readable', function() {
-    var chunk = process.stdin.read();
-    if (chunk !== null) {
-      displayMessage(`Your name is: ${chunk.trim()}`);
-    }
-  });
+displayMessage('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
 
-  process.stdin.on('end', function() {
-    displayMessage('This important software is now closing');
-  });
-}
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    displayMessage(`Your name is: ${chunk.trim()}`);
+  }
+});
 
-prompt();
+process.stdin.on('end', function() {
+  displayMessage('This important software is now closing');
+});
 
 module.exports = {};
