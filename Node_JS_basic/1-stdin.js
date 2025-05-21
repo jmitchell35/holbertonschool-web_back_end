bd905282cf76942e753b2f7c38e5a9d5a2e97fcd
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-21 12:33:15 +0200

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,22 @@
+const displayMessage = require('./0-console');
+
+function prompt() {
+  displayMessage('Welcome to Holberton School, what is your name?');
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('readable', function() {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+      chunk = chunk.trim();
+      displayMessage(`Your name is ${chunk}`);
+    }
+  });
+
+  process.stdin.on('end', function() {
+    displayMessage('This important software is now closing');
+  });
+}
+
+prompt();
+
+module.exports = prompt;
