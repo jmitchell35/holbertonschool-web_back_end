a5f27226a411c1902ec428079ddb9fb6970da247
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-21 12:53:43 +0200

    Removing displayMessage

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,12 @@
-const displayMessage = require('./0-console');
-
-
-displayMessage('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', function() {
   var chunk = process.stdin.read();
   if (chunk !== null) {
-    displayMessage(`Your name is: ${chunk.trim()}`);
+    process.stdout.write(`Your name is: ${chunk.trim()}`);
   }
 });
 
 process.stdin.on('end', function() {
-  displayMessage('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
-
-module.exports = {};
