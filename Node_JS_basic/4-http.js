450ebb0ee9d812557e484a189a9cf262f969dade
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-22 12:40:37 +0200

    Task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+// https://www.youtube.com/watch?v=dZ707QnDCmE
+
+const http = require('node:http');
+
+// req as request, res as response
+const app = http.createServer((_req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'texst/plain');
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+const port = 1245;
+
+app.listen(port);
