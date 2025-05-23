de3827a5bfdc4cc1224808d655867de13eaee334
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-23 15:05:53 +0200

    task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+// https://expressjs.com/en/starter/hello-world.html
+
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (_req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (_req, res) => {
+    try {
+      const students = await countStudents(process.argv[2]);
+      res.send(`This is the list of our students\n${students}`);
+    } catch (_error) {
+      res.send('Cannot load the database');
+    }
+})
+
+app.listen(port);
+
+module.exports = app;
